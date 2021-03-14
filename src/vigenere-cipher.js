becd6b6bb377c0240cde6990725ce0473a87a658
Onore <job.kyiv@gmail.com>
2021-03-14 15:47:19 +0200

    feat: implement Vigenere cipherr

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(prop = true) {
+    this.isReversed = prop;
+  }
+  // Ci = (Pi + Kj) mod26
+  encrypt(s, key) {
+    const letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+    key = key.toLowerCase().repeat(Math.ceil(s.length / key.length));
+
+    let counter = 0;
+    let res = s.split("").map((el, idx) => {
+      el = el.toLowerCase();
+
+      if (letters.findIndex((v) => v === el) === -1) {
+        counter++;
+        return el;
+      }
+
+      let fixIdx = counter > 0 ? idx - counter : idx;
+
+      let pj = letters.findIndex((v) => v === el);
+      let kj = letters.findIndex((v) => v === key[fixIdx]);
+
+      return letters[(pj + kj) % 26];
+    });
+
+    return this.isReversed
+      ? res.join("").toUpperCase()
+      : res.reverse().join("").toUpperCase();
+  }
+
+  // Ci = (Pi + 33 - Kj) mod 26
+  decrypt(s, key) {
+    const letters = "abcdefghijklmnopqrstuvwxyz".split("");
+    key = key.toLowerCase().repeat(Math.ceil(s.length / key.length));
+
+    let counter = 0;
+    let res = s.split("").map((el, idx) => {
+      el = el.toLowerCase();
+
+      if (letters.findIndex((v) => v === el) === -1) {
+        counter++;
+        return el;
+      }
+
+      let fixIdx = counter > 0 ? idx - counter : idx;
+
+      let pj = letters.findIndex((v) => v === el);
+      let kj = letters.findIndex((v) => v === key[fixIdx]);
+      return letters[(pj + 26 - kj) % 26];
+    });
+
+    return this.isReversed
+      ? res.join("").toUpperCase()
+      : res.reverse().join("").toUpperCase();
   }
 }
 
